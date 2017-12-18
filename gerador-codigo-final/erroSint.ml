291e1e81370292132df8b5080e257d373b500598
Guilherme Fernandes de Oliveira Martini <guiufo@gmail.com>
2017-12-18 10:49:43 -0200

    gerando para tutorial (faltam coisas)

--- gerador-codigo-final/erroSint.ml
+++ gerador-codigo-final/erroSint.ml
@@ -7,228 +7,224 @@ let message =
   fun s ->
     match s with
     | 0 ->
-        "<Erro: in\195\173cio do programa incorreto>\n"
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
     | 1 ->
-        "<Erro: ap\195\179s palavra reservada algoritmo>\n"
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
     | 4 ->
-        "<Erro: ap\195\179s palavra reservada algoritmo>\n"
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
     | 5 ->
-        "<Erro: declara\195\167\195\163o de vari\195\161veis incorreta>\n"
-    | 6 ->
-        "<Erro: declara\195\167\195\163o de vari\195\161veis incorreta>\n"
-    | 7 ->
-        "<Erro: declara\195\167\195\163o de vari\195\161veis incorreta>\n"
-    | 11 ->
-        "<Erro: declara\195\167\195\163o de vari\195\161veis incorreta>\n"
-    | 16 ->
-        "<Erro: declara\195\167\195\163o de vari\195\161veis incorreta>\n"
-    | 9 ->
-        "<Erro: declara\195\167\195\163o de vari\195\161veis incorreta>\n"
-    | 183 ->
-        "<Erro: ap\195\179s palavra reservada in\195\173cio>\n"
-    | 37 ->
-        "<Erro: comando se>\n"
-    | 96 ->
-        "<Erro: comando se>\n"
-    | 97 ->
-        "<Erro: comando se>\n"
-    | 169 ->
-        "<Erro: comando senao>\n"
-    | 172 ->
-        "<Erro: comando se>\n"
-    | 168 ->
-        "<Erro: comando se>\n"
-    | 39 ->
-        "<Erro: comando se>\n"
-    | 49 ->
-        "<Erro: comando se>\n"
-    | 47 ->
-        "<Erro: comando se>\n"
-    | 86 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 98 ->
-        "<Erro: ap\195\179s retorne>\n"
-    | 55 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 56 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 101 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 57 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 58 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 67 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 68 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 59 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 60 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 83 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 84 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 61 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 62 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 63 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 64 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 69 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 70 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 71 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 72 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 73 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 74 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 75 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 76 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 77 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 78 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 79 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 80 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 65 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 66 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 81 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 82 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 142 ->
-        "<Erro: ap\195\179s \"inicio\" bloco de comandos deve estar incorreto>\n"
-    | 184 ->
-        "<Erro: ap\195\179s \"inicio\" bloco de comandos deve estar incorreto>\n"
-    | 102 ->
-        "<Erro: comando para>\n"
-    | 104 ->
-        "<Erro: comando para>\n"
-    | 105 ->
-        "<Erro: comando para>\n"
-    | 106 ->
-        "<Erro: comando para>\n"
-    | 107 ->
-        "<Erro: comando para>\n"
-    | 108 ->
-        "<Erro: comando para>\n"
-    | 109 ->
-        "<Erro: comando para>\n"
-    | 110 ->
-        "<Erro: comando para>\n"
-    | 115 ->
-        "<Erro: comando para>\n"
-    | 112 ->
-        "<Erro: comando para>\n"
-    | 116 ->
-        "<Erro: comando para>\n"
-    | 166 ->
-        "<Erro: comando para>\n"
-    | 117 ->
-        "<Erro: comando leia>\n"
-    | 118 ->
-        "<Erro: comando leia>\n"
-    | 119 ->
-        "<Erro: comando leia>\n"
-    | 120 ->
-        "<Erro: comando leia>\n"
-    | 149 ->
-        "<Erro: ap\195\179s identificador>\n"
-    | 43 ->
-        "<Erro: ap\195\179s \"inicio\" bloco de comandos deve estar incorreto>\n"
-    | 150 ->
-        "<Erro: atribui\195\167\195\163o incorreta>\n"
-    | 151 ->
-        "<Erro: atribui\195\167\195\163o incorreta>\n"
-    | 44 ->
-        "<Erro: ap\195\179s \"inicio\" bloco de comandos deve estar incorreto>\n"
-    | 93 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 92 ->
-        "<Erro: express\195\163o incorreta>\n"
-    | 50 ->
-        "<Erro: ap\195\179s abrir colchetes>\n"
-    | 54 ->
-        "<Erro: ap\195\179s abrir colchetes>\n"
-    | 186 ->
-        "<Erro: ap\195\179s fimalgoritmo>\n"
-    | 122 ->
-        "<Erro: comando escreval>\n"
-    | 123 ->
-        "<Erro: comando escreval>\n"
-    | 125 ->
-        "<Erro: comando escreval>\n"
-    | 127 ->
-        "<Erro: comando escreva>\n"
-    | 128 ->
-        "<Erro: comando escreva>\n"
-    | 130 ->
-        "<Erro: comando escreva>\n"
-    | 132 ->
-        "<Erro: comando escolha>\n"
-    | 133 ->
-        "<Erro: comando escolha>\n"
-    | 134 ->
-        "<Erro: comando escolha>\n"
-    | 135 ->
-        "<Erro: comando escolha>\n"
-    | 164 ->
-        "<Erro: comando escolha>\n"
-    | 158 ->
-        "<Erro: comando escolha>\n"
-    | 161 ->
-        "<Erro: comando escolha>\n"
-    | 162 ->
-        "<Erro: comando escolha>\n"
-    | 136 ->
-        "<Erro: ap\195\179s palavra-chave enquanto>\n"
-    | 137 ->
-        "<Erro: ap\195\179s enquanto>\n"
-    | 138 ->
-        "<Erro: ap\195\179s comando faca>\n"
-    | 155 ->
-        "<Erro: Comando enquando sem fimenquanto >\n"
-    | 22 ->
-        "<Erro: ap\195\179s palavra-chave funcao>\n"
-    | 23 ->
-        "<Erro: ap\195\179s nome de uma fun\195\167\195\163o>\n"
-    | 24 ->
-        "<Erro: ap\195\179s abertura de paranteses dos par\195\162metros de uma fun\195\167\195\163o>\n"
-    | 25 ->
-        "<Erro: ap\195\179s identificador de par\195\162metro de uma fun\195\167\195\163o>\n"
-    | 26 ->
-        "<Erro: ap\195\179s \":\" dentro dos par\195\162metros de uma fun\195\167\195\163o>\n"
-    | 179 ->
-        "<Erro: ap\195\179s tipo de par\195\162metro de uma fun\195\167\195\163o>\n"
-    | 180 ->
-        "<Erro: entre par\195\162metros de uma fun\195\167\195\163o>\n"
-    | 30 ->
-        "<Erro: ap\195\179s fechamento do paranteses de uma fun\195\167\195\163o >\n"
-    | 35 ->
-        "<Erro: ap\195\179s declara\195\167\195\163o de vari\195\161veis de uma fun\195\167\195\163o>\n"
-    | 36 ->
-        "<Erro: ap\195\179s inicio de uma fun\195\167\195\163o>\n"
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
     | 174 ->
-        "<Erro: ap\195\179s retorno de uma fun\195\167\195\163o>\n"
-    | 188 ->
-        "<Erro: ap\195\179s fimfuncao>\n"
-    | 31 ->
-        "<Erro: ap\195\179s \":\" da declaracao de uma fun\195\167\195\163o>\n"
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 51 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 52 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 143 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 53 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 54 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 63 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 64 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 55 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 56 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 79 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 80 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 57 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 58 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 59 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 60 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 65 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 66 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 67 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 68 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 69 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 70 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 71 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 72 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 73 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 74 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 75 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 76 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 61 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 62 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 77 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 78 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 144 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 145 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 37 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 91 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 92 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 161 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 164 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 160 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 93 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 96 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 132 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 175 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 97 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 98 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 99 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 100 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 101 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 102 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 103 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 105 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 106 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 158 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 107 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 108 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 88 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 89 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 110 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 41 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 42 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 141 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 47 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 50 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 46 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 177 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 112 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 113 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 115 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 117 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 118 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 120 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 122 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 123 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 124 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 125 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 156 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 150 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 153 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 154 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 126 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 127 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 128 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 147 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 45 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 82 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 6 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 7 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 11 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 17 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 9 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 21 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 22 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 23 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 24 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 25 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 170 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 171 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 29 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 30 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 32 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
     | 33 ->
-        "<Erro: ap\195\179s tipo de retorno de uma fun\195\167\195\163o>\n"
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 35 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 166 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 179 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
+    | 34 ->
+        "<YOUR SYNTAX ERROR MESSAGE HERE>\n"
     | _ ->
         raise Not_found
